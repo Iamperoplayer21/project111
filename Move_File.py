1712453b5773cab739f6bbcab8deceeeb51db240
Iamperoplayer21 <100466792+Iamperoplayer21@users.noreply.github.com>
2023-04-27 16:47:49 +0530

    Update Move_File.py

--- Move_File.py
+++ Move_File.py
@@ -8,3 +8,9 @@ list_of_files = os.listdir(from_dir)
 
 print("List of files in the source directory:")
 print(list_of_files)
+
+for file_name in list_of_files:
+        name, extension = os.path.splitext(file_name)
+
+print("File name:", name)
+print("File extension:", extension)
